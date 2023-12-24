056274e0f7dccd9bc42581652f1db99f774bc7ab
Song0-0 <ks08094@naver.com>
2023-12-24 23:57:21 +0900

    주문검색기능 - 추후 querydsl 적용

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -19,8 +20,15 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    // 전체 조회시, 검색 조건 존재
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//
-//    }
+    //     전체 조회시, 검색 조건 존재
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return em.createQuery("select o from Order o join o.member m" +
+                                " where o.status = :status " +
+                                "and m.name like :name",
+                        Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1000) //최대 1000건
+                .getResultList();
+    }
 }
