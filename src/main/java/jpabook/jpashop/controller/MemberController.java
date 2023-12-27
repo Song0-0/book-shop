2f13d0ebf84b7d75530480f9e8bb6aac32ac588b
Song0-0 <ks08094@naver.com>
2023-12-28 00:31:10 +0900

    회원 목록 조회

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -38,5 +39,12 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/"; //저장이 끝나고나면 다시 재로딩되면 안좋아서 redirect를 주로 사용한다.redirect하면 첫페이지로 다시 간다.
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
